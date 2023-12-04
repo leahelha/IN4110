9d0558ca6827800011d1030f1a7892d14c42259e
leahelha <leahhhansen2000@gmail.com>
2023-12-04 17:13:33 +0100

    All done

--- assignment5/app.py
+++ assignment5/app.py
@@ -35,16 +35,30 @@ templates = Jinja2Templates(directory="templates")
 # - location_codes: location code dict
 # - today: current date
 
+
 @app.get("/")
 def strompris_template(request: Request):
+    """Render the 'strompris.html' template for the main page.
+
+    This function handles the main page request and renders the 'strompris.html' template.
+    It passes necessary data to the template, including the list of location codes and
+    the current date.
+
+    Args:
+        request (Request): The request object that includes details about the HTTP request.
+
+    Returns:
+        TemplateResponse: A template response that renders the 'strompris.html' template
+        with the provided input data. The input includes the request object, a list of
+        location codes, and the current date.
+    """
     inputs = {
         "request": request,
         "location_codes": LOCATION_CODES.keys(),
-        "today": datetime.date.today()
-
+        "today": datetime.date.today(),
     }
 
-    template = templates.TemplateResponse('strompris.html', inputs)
+    template = templates.TemplateResponse("strompris.html", inputs)
     return template
 
 
@@ -57,23 +71,48 @@ def strompris_template(request: Request):
 # produced by `plot_prices`
 # (task 5.6: return chart stacked with plot_daily_prices)
 
-@app.get('/plot_prices.json')
+
+@app.get("/plot_prices.json")
 def plot_prices_json(
     locations: Optional[List[str]] = Query(default=None),
     end: Optional[datetime.date] = None,
-    days: Optional[int] = 7
+    days: Optional[int] = 7,
 ):
-    
+    """Generate and return a Vega-Lite JSON chart of electricity prices.
+
+    This function fetches electricity price data based on the given parameters and
+    returns a Vega-Lite JSON chart. The chart illustrates prices over
+    time for selected locations.
+
+    Args:
+        locations (Optional[List[str]]): A list of location codes (["NO1", "NO2", "NO3", "NO4", "NO5"])
+            to include in the chart. If None, all locations are included. Defaults to None.
+        end (Optional[datetime.date]): The end date for the data range. If None, defaults
+            to the current date.
+        days (Optional[int]): The number of days before the end date to include in the
+            data range. Defaults to 7 days.
+
+    Returns:
+        dict: A dictionary representing a Vega-Lite JSON chart. This JSON structure describes
+        the chart visualizing electricity prices over the specified time period
+        and for the selected locations.
+
+    Note:
+        The `fetch_prices` and `plot_prices` functions from `strompris.py` are used to fetch
+        the data and generate the chart, respectively.
+    """
+
     # Getting the DataFrame produced through function defined in strompris.py
     df = fetch_prices(end, days, locations)
 
     # Getting chart from function defined in strompris.py
     chart = plot_prices(df)
 
-    # Converting chart 
+    # Converting chart
     json_chart = chart.to_dict()
 
     return json_chart
+
 
 # Task 5.6 (bonus):
 # `GET /activity` should render the `activity.html` template
@@ -85,8 +124,6 @@ def plot_prices_json(
 # - today: current date
 
 
-...
-
 # Task 5.6:
 # `GET /plot_activity.json` should return vega-lite chart JSON (alt.Chart.to_dict())
 # from `plot_activity_prices`
@@ -96,12 +133,7 @@ def plot_prices_json(
 # - minutes (int, default=10)
 
 
-...
-
-
 # mount your docs directory as static files at `/help`
-
-...
 
 
 def main():
--- assignment5/strompris.py
+++ assignment5/strompris.py
@@ -30,18 +30,24 @@ warnings.filterwarnings("ignore", ".*convert_dtype.*", FutureWarning)
 
 
 def fetch_day_prices(date: datetime.date = None, location: str = "NO1") -> pd.DataFrame:
-    """Fetch one day of data for one location from hvakosterstrommen.no API
+    """
+    Fetch one day of electricity price data for a specified location from the hvakosterstrommen.no API.
 
-    Function takes in date and location, and returns a dataframe of the electricitty prices for the date and location.
-    If date and location are not specified, the default date is today and location is "NO1" which is Oslo.
+    Function gathers the electricity price data for a single day at a given location in Norway. If no date is
+    provided, the date defaults to todays date. If no location is specified, it defaults
+    to "NO1" (Oslo). The function returns a DataFrame with the price in NOK per kWh,
+    start and end times of the price data, etc.
 
-    Parameters:
-    date [optional]: A datetime.date object containing the date in year, month, day format
-    location [optional]: A string, options are in the list ["NO1", "NO2", "NO3","NO4","NO5"] refering to locations in Norway
+    Args:
+        date (datetime.date, optional): The date for which to fetch the data, in year, month, day format.
+            Defaults to the current date if None.
+        location (str, optional): The location code for which to fetch the data. Valid options are
+            "NO1", "NO2", "NO3", "NO4", and "NO5", representing different regions in Norway.
+            Defaults to "NO1" (Oslo) if None.
 
     Returns:
-    df: A DataFrame containing electricity prices for the date
-
+        pd.DataFrame: A DataFrame containing electricity prices for the specified date and location.
+        The DataFrame includes columns for time start, time end, and price in NOK per kWh, EUR per kWh and  exchange rate EXR.
     """
     if date is None:
         date = datetime.date.today()  # Getting today's date unless otherwise specified
@@ -49,49 +55,72 @@ def fetch_day_prices(date: datetime.date = None, location: str = "NO1") -> pd.Da
     if location is None:
         location = "NO1"
 
-    year, month, day = date.year, date.strftime("%m"), date.strftime("%d")  # Splitting the date into year, month and day, %m and %d make sure we get a leading 0
-    
-    #print(date)
+    year, month, day = (
+        date.year,
+        date.strftime("%m"),
+        date.strftime("%d"),
+    )  # Splitting the date into year, month and day, %m and %d make sure we get a leading 0
+
+    # print(date)
     url = f"https://www.hvakosterstrommen.no/api/v1/prices/{year}/{month}-{day}_{location}.json"  # Defining right url
-    r = requests.get(url)    # Retrieving url
-    
+    r = requests.get(url)  # Retrieving url
+
     json_data = r.json()  # Converting text data from url to json structure
 
     # Making DataFrame
     df = pd.DataFrame(json_data)  # Converting json data to pandas DataFrame
-   
+
     # Convert 'time_start' and 'time_end' in the data files from UTC to 'Europe/Oslo' timezone
-    df['time_start'] = pd.to_datetime(df['time_start'], utc=True).dt.tz_convert("Europe/Oslo")
-    df['time_end'] = pd.to_datetime(df['time_end'], utc=True).dt.tz_convert("Europe/Oslo")
+    df["time_start"] = pd.to_datetime(df["time_start"], utc=True).dt.tz_convert(
+        "Europe/Oslo"
+    )
+    df["time_end"] = pd.to_datetime(df["time_end"], utc=True).dt.tz_convert(
+        "Europe/Oslo"
+    )
 
     return df
 
-    
 
 # LOCATION_CODES maps codes ("NO1") to names ("Oslo")
-LOCATION_CODES = {"NO1":"Oslo", "NO2":"Kristiansand", "NO3":"Trondheim", "NO4":"Tromsø", "NO5":"Bergen"}
+LOCATION_CODES = {
+    "NO1": "Oslo",
+    "NO2": "Kristiansand",
+    "NO3": "Trondheim",
+    "NO4": "Tromsø",
+    "NO5": "Bergen",
+}
 
 
 # task 1:
 
 
 def fetch_prices(
-    end_date: datetime.date = None, 
-    days: int = 7, 
-    locations: list[str] = tuple(LOCATION_CODES.keys())
-    ) -> pd.DataFrame:
-    """Fetch prices for multiple days and locations into a single DataFrame
-    Returns a DataFrame with the prices for a given time period.
+    end_date: datetime.date = None,
+    days: int = 7,
+    locations: list[str] = tuple(LOCATION_CODES.keys()),
+) -> pd.DataFrame:
+    """
+    Fetch electricity prices for multiple days and locations from hvakosterstrommen.no API.
 
-    If none of the parameters are specified, the default time period is 7 days from todays date in all locations in LOCATION_CODES.
+    This function gets electricity prices over a specified number of days leading up to an end date
+    for given locations. If no parameters are specified, it defaults to a 7-day period ending today,
+    for all locations in the dictionary LOCATION_CODES.
 
-    Parameters:
-    end_date [optional]: datetime.date object, containing end date of when you want your prices
-    days [optional]: int object, sets the number of days you want to retrieve from
-    locations [optional]: list or tuple, the location from where you want to fetch electricity prices
+    Args:
+        end_date (datetime.date, optional): The end date for the data range. Defaults to today's date
+            if None is provided.
+        days (int, optional): The number of days before the end date to include in the data range.
+            Defaults to 7.
+        locations (list[str], optional): A list of location codes where electricity prices can be fetched from.
+            The location codes should correspond to defined locations in the LOCATION_CODES. If None, prices for
+            all locations are fetched.
 
     Returns:
-    df: a DataFrame containing electricity prices over the given period
+        pd.DataFrame:  pd.DataFrame: A DataFrame containing electricity prices for the specified dates and locations.
+        The DataFrame includes columns for time start, time end, and price in NOK per kWh, EUR per kWh  exchange rate EXR, location code and location.
+
+    Note:
+        Prints message if the request to the API fails for any location on any date within the specified range.
     """
     if days is None:
         days = int(7)
@@ -102,43 +131,54 @@ def fetch_prices(
     if locations is None:
         locations = tuple(LOCATION_CODES.keys())
 
-    start_date = end_date - datetime.timedelta(days=days-1)
-    
-    all_data = []
+    start_date = end_date - datetime.timedelta(days=days - 1)
 
-    
+    all_data = []
 
     # Iterating over the dates
     for i in range(days):
-        date = start_date + datetime.timedelta(days=i)  
-        year, month, day = date.year, date.strftime("%m"), date.strftime("%d")  # Splitting the date into year, month and day, %m and %d make sure we get a leading 0
+        date = start_date + datetime.timedelta(days=i)
+        year, month, day = (
+            date.year,
+            date.strftime("%m"),
+            date.strftime("%d"),
+        )  # Splitting the date into year, month and day, %m and %d make sure we get a leading 0
 
         for location in locations:
-            #print(date)
+            # print(date)
             url = f"https://www.hvakosterstrommen.no/api/v1/prices/{year}/{month}-{day}_{location}.json"  # Defining right url
-            r = requests.get(url)    # Retrieving url
-            
+            r = requests.get(url)  # Retrieving url
+
             if r.status_code == 200:
                 json_data = r.json()  # Converting text data from url to json structure
 
-                df_j = pd.DataFrame(json_data) # Converting json data to pandas DataFrame
+                df_j = pd.DataFrame(
+                    json_data
+                )  # Converting json data to pandas DataFrame
 
-                 # Convert 'time_start' and 'time_end' in the data files from UTC to 'Europe/Oslo' timezone
-                df_j['time_start'] = pd.to_datetime(df_j['time_start'], utc=True).dt.tz_convert("Europe/Oslo")
-                df_j['time_end'] = pd.to_datetime(df_j['time_end'], utc=True).dt.tz_convert("Europe/Oslo")
+                # Convert 'time_start' and 'time_end' in the data files from UTC to 'Europe/Oslo' timezone
+                df_j["time_start"] = pd.to_datetime(
+                    df_j["time_start"], utc=True
+                ).dt.tz_convert("Europe/Oslo")
+                df_j["time_end"] = pd.to_datetime(
+                    df_j["time_end"], utc=True
+                ).dt.tz_convert("Europe/Oslo")
 
                 # Adding location
-                df_j['location_code'] = location
-                df_j['location'] = LOCATION_CODES[location]
+                df_j["location_code"] = location
+                df_j["location"] = LOCATION_CODES[location]
 
                 all_data.append(df_j)
 
             elif r.status_code != 200:
-                print(f'Oh no! {r.status_code}. For date = {date} and location = {location}')
-    
-    
+                print(
+                    f"Oh no! {r.status_code}. For date = {date} and location = {location}"
+                )
+
     # Making DataFrame with all data
-    df = pd.concat(all_data, ignore_index=True)  # Concatinating the list so that we can get all datas into one DataFrame
+    df = pd.concat(
+        all_data, ignore_index=True
+    )  # Concatinating the list so that we can get all datas into one DataFrame
 
     return df
 
@@ -147,35 +187,42 @@ def fetch_prices(
 
 
 def plot_prices(df: pd.DataFrame) -> alt.Chart:
-    """Plot energy prices over time and saves an html file of the plot.
+    """
+    Plot energy prices over time and save the plot as an HTML file.
 
-    x-axis is be time_start
-    y-axis is be price in NOK
-    each location gets its own line
+    This function creates a line chart using Altair, showing electricity
+    prices over time. The x-axis represents the time, and the y-axis represents the
+    price in NOK per kWh. Each location in the data is represented by a different line
+    in the chart. The chart is also saved as an HTML file for easy sharing and viewing.
 
     Parameters:
-    df: pd.DataFrame, containing the data for electricity prices over time. df is produced by fetch_prices
+        df (pd.DataFrame): A DataFrame containing electricity price data over time.
+            Expected to have columns 'time_start', 'NOK_per_kWh', and 'location'.
 
     Returns:
-    chart: alt.Chart, an altair chart which we can then show in a browser. Note the function also saves the chart as an html.
+        alt.Chart: An Altair Chart object. The chart visualizes the
+        electricity prices over time, with separate lines for different locations.
+        The function also saves this chart as 'Energy_prices_over_time.html'.
     """
-   # Plotting with altair
-    chart = alt.Chart(df).mark_line().encode(
-        x=alt.X('time_start:T',  axis=alt.Axis(title='Time')), # Time on x-axis
-        y=alt.Y('NOK_per_kWh:Q', axis=alt.Axis(title='Price [NOK per kWh]')),  # Price on y-axis
-        color='location:N',  # Different line color for each location
-        tooltip=['time_start:T', 'NOK_per_kWh:Q', 'location:N']
-    ).properties(
-        title='Energy prices over time in NOK',
-        width=800,
-        height=400
-    ).interactive()
+    # Plotting with altair
+    chart = (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x=alt.X("time_start:T", axis=alt.Axis(title="Time")),  # Time on x-axis
+            y=alt.Y(
+                "NOK_per_kWh:Q", axis=alt.Axis(title="Price [NOK per kWh]")
+            ),  # Price on y-axis
+            color="location:N",  # Different line color for each location
+            tooltip=["time_start:T", "NOK_per_kWh:Q", "location:N"],
+        )
+        .properties(title="Energy prices over time in NOK", width=800, height=400)
+        .interactive()
+    )
 
-    #saving to an html file
-    chart.save(f'Energy_prices_over_time.html')
+    # saving to an html file
+    chart.save(f"Energy_prices_over_time.html")
     return chart
-
-
 
 
 # Task 5.4
@@ -220,6 +267,7 @@ def plot_activity_prices(
 def main():
     """Allow running this module as a script for testing."""
     df = fetch_prices()
+
     chart = plot_prices(df)
     # showing the chart without requiring jupyter notebook or vs code for example
     # requires altair viewer: `pip install altair_viewer`
